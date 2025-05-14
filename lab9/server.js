79d68322bb662c28822a752ec340d76bde33c5de
re4vik <138435322+re4vik@users.noreply.github.com>
2025-05-14 23:04:33 +0300

    Create server.js

---
+++ lab9/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+// Налаштування підключення до бази даних PostgreSQL
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ya ne vigadav sho smishne napisaty',
+  password: '123',
+  port: 5432,
+});
+
+// Маршрут для перевірки підключення до бази даних
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT NOW()');
+    res.send(result.rows);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send('Error ' + err);
+  }
+});
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Сервер запущено на http://localhost:${port}`);
+});
